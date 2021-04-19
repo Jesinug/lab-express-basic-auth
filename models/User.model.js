09092ca1e69ab45312a1a3f5c96c2f1114031a7d
Jesica Nugnez <jesica.nugnez@gmail.com>
2021-04-19 15:52:01 +0200

    Iteration 1 done

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,14 @@
-// User model here
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//generando el Schema userSchema (collection)
+const userSchema = new Schema ({ 
+    username: {type: String, require: true, unique: true},
+    password: { type: String, require: true }
+ });
+
+ //generando el modelo User (DB)
+ const User = mongoose.model('User', userSchema);
+
+ module.exports = User;
+
